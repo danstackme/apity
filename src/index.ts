55ef28c5e0b537d47b11c6e2d8567c2b8f2d91c7
Daniel Osmond <dosmond37@gmail.com>
2025-03-17 14:56:06 -0600

    Correct export plugin

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { useFetch, useMutate } from "./hooks";
 export { ApiProvider, useApiContext } from "./context";
 export type { ApiEndpoint, HttpMethod } from "./types";
 export { createApiEndpoint, createApi } from "./createApi";
+export { apityPlugin } from "./cli/vite-plugin";
